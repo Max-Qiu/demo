5e84ed3deb6aab680d17b7782a79b76b3487876f
Max-Qiu <qzhing@126.com>
2022-07-31 19:05:45 +0800

    读写锁

--- demo-juc/src/main/java/com/maxqiu/demo/ReadWriteLock/TestReadWriteLock.java
+++ demo-juc/src/main/java/com/maxqiu/demo/ReadWriteLock/TestReadWriteLock.java
@@ -1,13 +1,13 @@
 package com.maxqiu.demo.ReadWriteLock;
 
 /**
- * 
+ *
  * ReadWriteLock:=读写锁<br>
  * <br>
  * 写写/读写 需要“互斥”<br>
  * <br>
  * 读读 不需要互斥
- * 
+ *
  * @author Max_Qiu
  */
 public class TestReadWriteLock {
@@ -18,7 +18,7 @@ public class TestReadWriteLock {
         new Thread(() -> entity.setNumber((int)(Math.random() * 101)), "Write").start();
 
         for (int i = 0; i < 50; i++) {
-            new Thread(() -> entity.getNumber()).start();
+            new Thread(entity::getNumber).start();
             // 不要写成下面这样，写了就看不出效果了（可能是::效率有点低）
             // new Thread(entity::getNumber).start();
         }
